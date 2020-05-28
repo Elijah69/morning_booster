31dec1010be194095894ac4c154972d932fdf997
elijah69 <elijahnightingale@gmail.com>
2020-05-28 17:55:52 +0300

    Developed logic for timers and added a transfer for timers from main screen to timers screen

--- app/src/main/java/com/lifebestpractices/morningbooster/MainActivity.kt
+++ app/src/main/java/com/lifebestpractices/morningbooster/MainActivity.kt
@@ -2,10 +2,25 @@ package com.lifebestpractices.morningbooster
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.lifebestpractices.morningbooster.databinding.ActivityMainBinding
+import java.io.Serializable
+
+const val TAG = "MBD"
+const val INTENT_PARAM_KEY_FIRST_TIMER = "1 timer"
+const val INTENT_PARAM_KEY_SECOND_TIMER = "2 timer"
+const val INTENT_PARAM_KEY_THIRD_TIMER = "3 timer"
+const val INTENT_PARAM_KEY_FOURTH_TIMER = "4 timer"
+const val INTENT_PARAM_KEY_FIFTH_TIMER = "5 timer"
+const val INTENT_PARAM_KEY_SIXTH_TIMER = "6 timer"
+
+class PractiseTimer constructor(val name: String, var position : Int) : Serializable{
+    var minutes : Long = 60000
+    var enabled : Boolean = true
+    fun setNewTime(newMinutes : Int){
+        minutes = (newMinutes * 60000).toLong()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,27 +28,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-        val affirmationTime = 7
-        val visualizationTime = 8
-        val physicalTime = 9
-        val readingTime = 11
-        val silenceTime = 12
-        val diaryTime = 13
-        mainBinding.affirmationTime.text = "$affirmationTime minutes"
-        mainBinding.visualizationTime.text = "$visualizationTime minutes"
-        mainBinding.physicalTime.text = "$physicalTime minutes"
-        mainBinding.readingTime.text = "$readingTime minutes"
-        mainBinding.silenceTime.text = "$silenceTime minutes"
-        mainBinding.diaryTime.text = "$diaryTime minutes"
+        val firstTimer = PractiseTimer("affirmation", 2)
+        firstTimer.setNewTime(1)
+        val secondTimer = PractiseTimer("visualization", 1)
+        secondTimer.setNewTime(1)
+        val thirdTimer = PractiseTimer("physical", 3)
+        thirdTimer.setNewTime(1)
+        thirdTimer.enabled = false
+        val fourthTimer = PractiseTimer("reading", 4)
+        fourthTimer.setNewTime(1)
+        val fifthTimer = PractiseTimer("silence", 5)
+        fifthTimer.setNewTime(1)
+        val sixthTimer = PractiseTimer("diary", 6)
+        sixthTimer.setNewTime(1)
+        val timers: MutableList<PractiseTimer> = mutableListOf(firstTimer, secondTimer, thirdTimer, fourthTimer, fifthTimer, sixthTimer)
+        timers.sortBy {
+            it.position
+        }
+        mainBinding.affirmationTime.text = "${timers[0].minutes / 60000} minutes"
+        mainBinding.visualizationTime.text = "${timers[1].minutes / 60000} minutes"
+        mainBinding.physicalTime.text = "${timers[2].minutes / 60000} minutes"
+        mainBinding.readingTime.text = "${timers[3].minutes / 60000} minutes"
+        mainBinding.silenceTime.text = "${timers[4].minutes / 60000} minutes"
+        mainBinding.diaryTime.text = "${timers[5].minutes / 60000} minutes"
 
         mainBinding.boostButton.setOnClickListener {
             val intent = Intent(this, TimerActivity::class.java).apply {
-                putExtra("affirmation time", affirmationTime)
-                putExtra("visualization time", visualizationTime)
-                putExtra("physical time", physicalTime)
-                putExtra("reading time", readingTime)
-                putExtra("silence time", silenceTime)
-                putExtra("diary time", diaryTime)
+                putExtra(INTENT_PARAM_KEY_FIRST_TIMER, firstTimer)
+                putExtra(INTENT_PARAM_KEY_SECOND_TIMER, secondTimer)
+                putExtra(INTENT_PARAM_KEY_THIRD_TIMER, thirdTimer)
+                putExtra(INTENT_PARAM_KEY_FOURTH_TIMER, fourthTimer)
+                putExtra(INTENT_PARAM_KEY_FIFTH_TIMER, fifthTimer)
+                putExtra(INTENT_PARAM_KEY_SIXTH_TIMER, sixthTimer)
             }
             startActivity(intent)
         }
--- app/src/main/java/com/lifebestpractices/morningbooster/TimerActivity.kt
+++ app/src/main/java/com/lifebestpractices/morningbooster/TimerActivity.kt
@@ -3,7 +3,7 @@ package com.lifebestpractices.morningbooster
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.Toast
+import android.util.Log
 import com.lifebestpractices.morningbooster.databinding.ActivityTimerBinding
 
 class TimerActivity : AppCompatActivity() {
@@ -12,32 +12,54 @@ class TimerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val timerBinding = ActivityTimerBinding.inflate(layoutInflater)
         setContentView(timerBinding.root)
-        val affirmationTime = intent.getIntExtra("affirmation time", 10)
-        val visualizationTime = intent.getIntExtra("visualization time", 10)
-        val physicalTime = intent.getIntExtra("physical time", 10)
-        val readingTime = intent.getIntExtra("reading time", 10)
-        val silenceTime = intent.getIntExtra("silence time", 10)
-        val diaryTime = intent.getIntExtra("diary time", 10)
-        createCountDownTimer("affirmation", affirmationTime)
-
+        val timers: MutableList<PractiseTimer> = getAndPrepareTimers()
+        startTimers(timers, timerBinding)
     }
 
-    private fun createCountDownTimer(timerType: String,affirmationTime: Int): CountDownTimer {
-        val toMinutes = 60000
-        return object : CountDownTimer(affirmationTime.toLong() * toMinutes, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                val remainingMinutes = millisUntilFinished / toMinutes
-                val remainingSeconds = (millisUntilFinished / 1000) % 60
-                Toast.makeText(
-                    this@TimerActivity,
-                    "Second for $timerType remaining: $remainingMinutes : $remainingSeconds",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+    private fun getAndPrepareTimers(): MutableList<PractiseTimer> {
+        val firstTimer = intent.getSerializableExtra(INTENT_PARAM_KEY_FIRST_TIMER) as PractiseTimer
+        val secondTimer =
+            intent.getSerializableExtra(INTENT_PARAM_KEY_SECOND_TIMER) as PractiseTimer
+        val thirdTimer = intent.getSerializableExtra(INTENT_PARAM_KEY_THIRD_TIMER) as PractiseTimer
+        val forthTimer = intent.getSerializableExtra(INTENT_PARAM_KEY_FOURTH_TIMER) as PractiseTimer
+        val fifthTimer = intent.getSerializableExtra(INTENT_PARAM_KEY_FIFTH_TIMER) as PractiseTimer
+        val sixthTimer = intent.getSerializableExtra(INTENT_PARAM_KEY_SIXTH_TIMER) as PractiseTimer
+        val timers: MutableList<PractiseTimer> =
+            mutableListOf(firstTimer, secondTimer, thirdTimer, forthTimer, fifthTimer, sixthTimer)
+        timers.removeAll {
+            !it.enabled
+        }
+        timers.sortBy {
+            it.position
+        }
+        return timers
+    }
 
-            override fun onFinish() {
-                Toast.makeText(this@TimerActivity, "done!", Toast.LENGTH_SHORT).show()
-            }
+    private fun startTimers(timers: MutableList<PractiseTimer>, timerBinding: ActivityTimerBinding) {
+        if (timers.isNotEmpty()) {
+            Log.d(TAG, "Start ${timers.first().name} timer")
+            object : CountDownTimer(timers.first().minutes, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    timerBinding.timerTitle.text = timers.first().name
+                    val text = getString(R.string.timer, remainingMinutes(millisUntilFinished), remainingSeconds(millisUntilFinished))
+                    timerBinding.timer.text = text
+                }
+
+                override fun onFinish() {
+                    timers.removeAt(0)
+                    startTimers(timers, timerBinding)
+                }
+            }.start()
+        } else {
+            timerBinding.timerTitle.text = "Done"
+            timerBinding.timer.text = ""
+            Log.d(TAG, "The timers finished")
         }
     }
+
+    private fun remainingMinutes(millisUntilFinished: Long) =
+        millisUntilFinished / 60000
+
+    private fun remainingSeconds(millisUntilFinished: Long) =
+        (millisUntilFinished / 1000) % 60
 }
