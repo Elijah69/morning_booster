f75b47888a3b1c85f0d84c7aae3757bd1e430a21
Elijah69 <elijahnightingale@gmail.com>
2021-01-19 13:37:37 +0300

    added iOS project

---
+++ morningboosterIOS/morningboosterIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  morningboosterIOS
+//
+//  Created by Ilia.Solovei on 19/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ morningboosterIOS/morningboosterIOS/morningboosterIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  morningboosterIOSApp.swift
+//  morningboosterIOS
+//
+//  Created by Ilia.Solovei on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct morningboosterIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
