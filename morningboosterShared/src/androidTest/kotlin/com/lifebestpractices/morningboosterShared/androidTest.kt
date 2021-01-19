19bc8073878fdee4f7efe43ce80bdd4889387fad
Elijah69 <elijahnightingale@gmail.com>
2021-01-19 14:00:29 +0300

    added a shared lib for multiplatform develop

---
+++ morningboosterShared/src/androidTest/kotlin/com/lifebestpractices/morningboosterShared/androidTest.kt
@@ -0,0 +1,12 @@
+package com.lifebestpractices.morningboosterShared
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class GreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue("Check Android is mentioned", Greeting().greeting().contains("Android"))
+    }
+}
---
+++ morningboosterShared/src/commonMain/kotlin/com/lifebestpractices/morningboosterShared/Platform.kt
@@ -0,0 +1,5 @@
+package com.lifebestpractices.morningboosterShared
+
+expect class Platform() {
+    val platform: String
+}
